b77ab9ae8fedf75060b426b55a7520ff7935474c
Israel Soares <israeldiassoares@gmail.com>
2019-04-08 06:48:52 -0300

    create checkout

--- js/app.js
+++ js/app.js
@@ -90,6 +90,14 @@ new Vue({
       if (index !== -1) {
         this.cart.items.splice(index, 1);
       }
+    },
+    checkout: function () {
+      if (confirm('Are you sure that you want purchase these products? ')) {
+        this.cart.items.forEach(function (item) {
+          item.product.inStock += item.quantity;
+        });
+        this.cart.items = [];
+      }
     }
 
   },
